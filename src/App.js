bc562da37c250080dbf45b24db4f741fbeeafc2f
Kristina <yarevyakinakristina@gmail.com>
2023-12-06 19:36:20 +0300

    fix: added default tasks

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ import Footer from './components/footer/Footer';
 import Main from './components/main/Main';
 import { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import data from './mock.json';
 
 function App() {
+    const storedTasks = JSON.parse(window.localStorage.getItem('tasks'));
+    const initialState = storedTasks || data;
+    const [tasks, setTasks] = useState(initialState);
 
-  const initialState = JSON.parse(window.localStorage.getItem('tasks')) || []
-  const [tasks, setTasks] = useState(initialState)
+    useEffect(() => {
+        window.localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
 
-	useEffect(() => {
-		window.localStorage.setItem('tasks', JSON.stringify(tasks))
-	}, [tasks])
-
-  return (
-    <BrowserRouter>
-    <div>
-      <Header/>
-			<Main tasks={tasks} setTasks={setTasks} />
-			<Footer tasks={tasks} />
-    </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Main tasks={tasks} setTasks={setTasks} />
+                <Footer tasks={tasks} />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
