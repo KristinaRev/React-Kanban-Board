76d974590123136c4f42f7b3204cde64163cedcd
Kristina <yarevyakinakristina@gmail.com>
2024-04-06 20:17:02 +0300

    feat: add Login to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,14 @@
 import React, { memo } from 'react';
-import Button from '../button/Button';
+import Login from "../Login/Login";
 import css from './Header.module.css';
 
 function Header({ onLogin, onLogout, user }) {
 
-	const loggedInContent = (
-		<>
-			<p className={css.header_user}>Welcome, {user}!</p>
-			<Button onClick={onLogout} className={css.header_btn}>
-				Log Out
-			</Button>
-		</>
-	);
-
-	const loggedOutContent = (
-		<Button onClick={onLogin} className={css.header_btn}>
-			Log In
-		</Button>
-	);
-
 	return (
 		<header className={css.header}>
 			<h1 className={css.header_title}>Awesome Kanban Board</h1>
-			{user ? loggedInContent : loggedOutContent}
+			{user ? <p className={css.header_user}>Welcome, {user}!</p> : ''}
+			<Login onLogin={onLogin} onLogout={onLogout} user={user}/>
 		</header>
 	);
 }
