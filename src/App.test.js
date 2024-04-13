bb4d6c57c55b9b84e70c3f406467b7fd4eddd043
Kristina <yarevyakinakristina@gmail.com>
2024-04-13 13:19:18 +0300

    refactor

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Login from "./components/Login/Login";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Login', () => {
+
+  test('toggles dropdown on click', async () => {
+    const { getByAltText, findByText } = render(<Login />);
+    const userAvatar = getByAltText('user avatar');
+    fireEvent.click(userAvatar);
+    const profileButton = await findByText(/Profile/i);
+    expect(profileButton).toBeInTheDocument();
+  });
+
+  test('performs login action', async () => {
+    const onLoginMock = jest.fn();
+    const { getByText, getByAltText, queryByText } = render(<Login onLogin={onLoginMock} />);
+    const userAvatar = getByAltText('user avatar');
+    fireEvent.click(userAvatar);
+    const loginButton = queryByText(/Log In/i);
+    fireEvent.click(loginButton);
+    await waitFor(() => {
+      expect(onLoginMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test('performs logout action', async () => {
+    const onLogoutMock = jest.fn();
+    const { getByText, getByAltText, queryByText } = render(<Login user={{}} onLogout={onLogoutMock} />);
+    const userAvatar = getByAltText('user avatar');
+    fireEvent.click(userAvatar);
+    const logoutButton = queryByText(/Log Out/i);
+    fireEvent.click(logoutButton);
+    await waitFor(() => {
+      expect(onLogoutMock).toHaveBeenCalledTimes(1);
+    });
+  });
 });
