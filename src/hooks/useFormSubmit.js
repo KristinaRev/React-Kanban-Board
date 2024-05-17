fcb36b5ecc0563ff396639ad0ddba4a505b12fc7
Kris Kipper <yarevyakinakristina@gmail.com>
2024-05-17 05:58:21 -0600

    clean: delete unnecessary code

--- src/hooks/useFormSubmit.js
+++ src/hooks/useFormSubmit.js
@@ -1,41 +1,7 @@
-import React, {useCallback} from 'react';
-import { useId } from 'react-id-generator';
-import uniqid from "uniqid";
+import React from 'react';
 
 
 /**
  * Хук для создания новой задачи
  */
-export function useFormSubmit({ tasks, setTasks, title, description, userId, updatedTasks }, callback) {
 
-    return useCallback((title, description, userId) => {
-        const newTask = {
-            id: uniqid(),
-            title,
-            description,
-            created: new Date().toISOString(),
-            status: 'backlog',
-        };
-
-        fetch('http://localhost:3001/tasks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newTask),
-        }).then(async (response) => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-
-            const data = await response.json();
-
-            callback(data);
-        }).catch((error) => {
-            console.error('Error adding task:', error.message);
-        });
-
-        setTasks(prevTasks => [...prevTasks, newTask]);
-
-    }, [userId, callback]);
-}
