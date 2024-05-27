88ea31ba17ebb89fd57feb2d24058855932c51fc
Kris Kipper <yarevyakinakristina@gmail.com>
2024-05-27 16:55:32 +0300

    feat: rewrite footer to ts

---
+++ declarations.d.ts
@@ -0,0 +1,4 @@
+declare module '*.module.css' {
+    const classes: { [key: string]: string | number};
+    export default classes;
+}
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { observer } from "mobx-react-lite";
+import './Footer.scss';
+
+interface FooterProps {
+	backlogCount: number;
+	doneCount: number;
+	inProgressCount: number;
+	readyCount: number;
+}
+
+const Footer: React.FC<FooterProps> = ({ backlogCount, doneCount, inProgressCount, readyCount }) => {
+	return (
+		<footer className="footer">
+			<div className="counts">
+				<p className="count">backlog: {backlogCount}</p>
+				<p className="count">done: {doneCount}</p>
+				<p className="count">inProgress: {inProgressCount}</p>
+				<p className="count">ready: {readyCount}</p>
+			</div>
+			<div className="copy">
+				Created by{' '}
+				<a
+					href='https://github.com/KristinaRev'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Kris Kipper
+				</a>
+				, 2023
+			</div>
+		</footer>
+	);
+}
+
+export default React.memo(observer(Footer));
