01999214dd9c5c1adc12598df00c958f78140873
Kris Kipper <yarevyakinakristina@gmail.com>
2024-06-28 15:06:43 +0300

    refactor: move utils.ts to utils/helpers

--- src/stores/users.store.ts
+++ src/stores/users.store.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable } from 'mobx';
 import uniqid from 'uniqid';
 import React from 'react';
-import { formatDate } from '../utils';
+import { formatDate } from '../utils/helpers/utils';
 
 interface User {
   id: string;
