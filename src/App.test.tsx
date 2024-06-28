909c01d394f4219d87f84b9f5e3be55ee26bd1ab
KrisKipper <144679096+KrisKipper@users.noreply.github.com>
2024-06-28 15:02:10 +0300

    Рефакторинг:  компоненты переписаны на ts (#22)
    * refactor: rewrite to ts
    
    * fix: tests logout
    
    ---------
    
    Co-authored-by: Kris Kipper <yarevyakinakristina@gmail.com>

---
+++ src/App.test.tsx
@@ -0,0 +1,61 @@
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Login from './components/Login/Login';
+import { RootStore, StoreContext } from './stores/root.store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ReactNode } from 'react';
+import { TasksStore } from './stores/tasks.store';
+import { UsersStore } from './stores/users.store';
+
+describe('Login', () => {
+  let rootStore: RootStore;
+
+  beforeEach(() => {
+    rootStore = new RootStore();
+  });
+
+  const renderWithContext = (component: ReactNode) => {
+    return render(
+      <StoreContext.Provider value={rootStore}>
+        <Router>{component}</Router>
+      </StoreContext.Provider>
+    );
+  };
+
+  test('toggles dropdown on click', async () => {
+    const { getByAltText, findByText } = renderWithContext(<Login />);
+    const userAvatar = getByAltText('user avatar');
+    if (userAvatar) {
+      fireEvent.click(userAvatar);
+    }
+    const profileButton = await findByText(/Profile/i);
+    expect(profileButton).toBeInTheDocument();
+  });
+
+  test('performs login action', async () => {
+    const { getByAltText, queryByText } = renderWithContext(<Login />);
+    const userAvatar = getByAltText('user avatar');
+    if (userAvatar) {
+      fireEvent.click(userAvatar);
+    }
+    const loginButton = queryByText(/Log In/i);
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test('performs logout action', async () => {
+    rootStore.usersStore.login = true;
+    const onLogoutMock = jest.fn();
+    const { getByAltText, queryByText } = renderWithContext(<Login onLogout={onLogoutMock} />);
+    const userAvatar = getByAltText('user avatar');
+    if (userAvatar) {
+      fireEvent.click(userAvatar);
+    }
+    const logoutButton = queryByText(/Log Out/i);
+    if (logoutButton) {
+      fireEvent.click(logoutButton);
+    }
+    await waitFor(() => {
+      expect(onLogoutMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
