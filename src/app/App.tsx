9adc369295870c2bb887b3286e8605f65bba9965
KrisKipper <144679096+KrisKipper@users.noreply.github.com>
2024-06-28 17:26:49 +0300

    refactor: move apps files to app (#25)
    Co-authored-by: Kris Kipper <yarevyakinakristina@gmail.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import RoutingPage from './pages/Routing-page/Routing.page';
+import RoutingPage from '../pages/Routing-page/Routing.page';
 import { observer } from 'mobx-react-lite';
 import './App.css';
 
